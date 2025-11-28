078b16ab42e712aba1708f790f85a3c43c085a86
Maruf2309 <maruf.hossain.1997@gmail.com>
2025-11-28 23:04:48 +0600

    list added

---
+++ PYTHON-PROGRAMMING/Lecture2_PipModule/custom.py
@@ -0,0 +1,14 @@
+
+# Custome Module
+
+def greeting(name):
+    return f"Hello, {name}"
+
+def add(a, b):
+    return a + b
+
+    """ 
+    Custome Function written in custom.py file
+    where its imported from main.py file as package
+
+    """
---
+++ PYTHON-PROGRAMMING/Lecture2_PipModule/main.py
@@ -0,0 +1,12 @@
+
+import custom
+print(custom.greeting("Maruf"))
+print(custom.add(5,7))
+
+
+
+""" 
+    Custome Function written in custom.py file
+    where its imported from main.py file as package
+
+"""
